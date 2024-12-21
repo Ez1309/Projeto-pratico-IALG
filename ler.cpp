636b6a365ff0619023b5055b8b48a3ef191f81b3
Ezequiel <eze.dmgz06@gmail.com>
2024-12-21 17:43:03 -0300

    atualizacao da branch master com a leitura de arquivos binarios

--- ler.cpp
+++ ler.cpp
@@ -3,77 +3,165 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
-
-
-#include "menu.cpp"
+#include <cstring>
+#include <typeinfo>
 
 using namespace std;
 
-struct veiculo{
-    char *placa = new char[8];
-    char *fabricante = new char[14];
-    char *modelo = new char[9];
-    char *cor = new char[9];
+const int tamanhoPlaca = 8;
+const int tamanhoFabricante = 14;
+const int tamanhoModelo = 9;
+const int tamanhoCor = 9;
+const int tamanhoCategoria = 16;
+const int tamanhoDescricao = 166;
+const int tamanhoDisponibilidade = 12;
+const int tamanhoLocador = 21;
+
+struct veiculo {
+    char placa[tamanhoPlaca];
+    char fabricante[tamanhoFabricante];
+    char modelo[tamanhoModelo];
+    char cor[tamanhoCor];
     int ano;
     int quilometragem;
-    char *categoria = new char[16];
-    char *descricao = new char [166];
+    char categoria[tamanhoCategoria];
+    char descricao[tamanhoDescricao];
     double preco;
-    char *disponibilidade = new char[12];
-    char *locador = new char[21];
+    char disponibilidade[tamanhoDisponibilidade];
+    char locador[tamanhoLocador];
 };
 
-int main(){
+bool csvParaBinario(int &linhas);
+bool lerDados(veiculo* &carros, int &linhas);
+void imprimirTabela(veiculo carros[], int &linhas);
 
-    menu();
-    ifstream arquivo("carros.csv");
+bool csvParaBinario(int &linhas){
+
+    bool leuComSucesso = true;
+
+    // Criação ou atualização do arquivo binário
+    ofstream arquivoBinario("carros.dat");
+    if (!arquivoBinario){
+        cout << "Não foi possível escrever no arquivo carros.dat!" << endl;
+        return(not leuComSucesso);
+    }
+
+    // Abertura do arquivo CSV
+    ifstream arquivoCSV("carros.csv");
+    if (!arquivoCSV){
+        cout << "Não foi possível abrir o arquivo carros.csv!" << endl;
+        return (not leuComSucesso);
+    }
+    
+    // Ignorar cabeçalho do CSV
     string linha;
-    getline(arquivo, linha);
+    getline(arquivoCSV, linha);
+    
 
-    int tamanho = 0;
-    veiculo *carros;
-    carros = new veiculo[40];
+    linhas = 0;
+    cout << "Linha " << linhas+1 << ": "<< linha << endl << endl;
+    // Loop para preencher o arquivo binário com os carros
+    while(!arquivoCSV.eof()){
+        cout << "Linha " << linhas+2 << ": ";
+        veiculo carro = {}; // Variável temporária
 
-    // Ler os dados até chegar ao fim do arquivo
-    while(!arquivo.eof()){
-        // Condicional para redimensionar o vetor caso ele tenha mais de 40 elementos
-        if (tamanho >= 40){
-            veiculo *temp = new veiculo [tamanho + 1];
-            copy(carros, carros+tamanho, temp);
-            delete [] carros;
-            carros = temp;
-        }
         // Leitura da placa, fabricante, modelo e cor
-        arquivo.getline(carros[tamanho].placa, 8, ',');
-        arquivo.getline(carros[tamanho].fabricante, 14, ',');
-        arquivo.getline(carros[tamanho].modelo, 9, ',');
-        arquivo.getline(carros[tamanho].cor, 9, ',');
+        arquivoCSV.getline(carro.placa, tamanhoPlaca, ',');
+        cout << carro.placa << ", ";
+
+        arquivoCSV.getline(carro.fabricante, tamanhoFabricante, ',');
+        cout << carro.fabricante << ", ";
+
+        arquivoCSV.getline(carro.modelo, tamanhoModelo, ',');
+        cout << carro.modelo << ", ";
+
+        arquivoCSV.getline(carro.cor, tamanhoCor, ',');
+        cout << carro.cor << ", ";
 
         // Leitura do ano ignorando a vírgula
-        arquivo >> carros[tamanho].ano;
-        arquivo.ignore();
+        arquivoCSV >> carro.ano;
+        arquivoCSV.ignore();
+        cout << carro.ano << ", ";
 
         // Leitura da quilometragem ignorando a vírgula
-        arquivo >> carros[tamanho].quilometragem;
-        arquivo.ignore();
+        arquivoCSV >> carro.quilometragem;
+        arquivoCSV.ignore();
+        cout << carro.quilometragem << ", ";
 
         // Leitura da categoria
-        arquivo.getline(carros[tamanho].categoria, 16, ',');
+        arquivoCSV.getline(carro.categoria, tamanhoCategoria, ',');
+        cout << carro.categoria << ", ";
 
-        // Leitura da descrição ignorando as primeiras aspas " e a vírgula
-        arquivo.ignore();
-        arquivo.getline(carros[tamanho].descricao, 166, '"');
-        arquivo.ignore();
+        // Leitura da descrição
+        arquivoCSV.getline(carro.descricao, tamanhoDescricao, ',');
+        cout << carro.descricao << ", ";
 
         // Leitura do preço ignorando a vírgula
-        arquivo >> carros[tamanho].preco;
-        arquivo.ignore();
+        arquivoCSV >> carro.preco;
+        arquivoCSV.ignore();
+        cout << carro.preco << ", ";
 
         // Leitura da disponibilidade e do locador
-        arquivo.getline(carros[tamanho].disponibilidade, 12, ',');
-        arquivo.getline(carros[tamanho].locador, 21, '\n');
-        tamanho++;
+        arquivoCSV.getline(carro.disponibilidade, tamanhoDisponibilidade, ',');
+        cout << carro.disponibilidade << ", ";
+
+        arquivoCSV.getline(carro.locador, tamanhoLocador, '\n');
+        cout << carro.locador << endl << endl;
+
+        // Escrita do carro no arquivo binário
+        arquivoBinario.write((const char*)(&carro), sizeof(veiculo));
+        linhas++;
     }
+
+    cout << endl << "Quantidade de linhas lidas: " << linhas << endl << endl;
+
+    cout << "Linhas depois da escrita: " << linhas << endl;
+
+    // Fechando os arquivos abertos
+    arquivoCSV.close();
+    arquivoBinario.close();
+
+    return leuComSucesso;
+}
+
+bool lerDados(veiculo* &carros, int &linhas){
+
+    bool leuComSucesso = true;
+
+    // Abertura do arquivo binário contendo os carros
+    ifstream arquivoBinario("carros.dat");
+    if(!arquivoBinario){
+        cout << "Não foi possível abrir o arquivo carros.dat!" << endl;
+        return (not leuComSucesso);
+    }
+
+    int tamanhoVetor = 40;
+    int qtdCarros = 0;
+
+    for (int i=0; i<linhas; i++){
+        // Redimensionando o vetor de carros
+        if (qtdCarros == tamanhoVetor){
+            cout << "Redimensionando vetor de " << tamanhoVetor << " para " << tamanhoVetor + 10 << endl;
+            int novoTamanho = tamanhoVetor + 10;
+            veiculo *temp = new veiculo[novoTamanho];
+            copy(carros, carros+tamanhoVetor, temp);
+            delete[] carros;
+            carros = temp;
+            tamanhoVetor = novoTamanho;
+        }
+
+        // Leitura do carro
+        arquivoBinario.read((char *)(&carros[i]), sizeof(veiculo));
+        qtdCarros++;
+    }
+
+    // Fechando o arquivo binário
+    arquivoBinario.close();
+
+    return leuComSucesso;
+}
+
+void imprimirTabela(veiculo carros[], int &tamanho){
 
     int colBarra = 1;
     int colPlaca = 9;
@@ -87,79 +175,76 @@ int main(){
     int colDisponibilidade = 17;
     int colLocador = 20;
 
-    // ╭ ╮ ╰ ╯ ┬ ┴ ┼ ─
-
     // Códigos ANSI para negrito
-    const string bold_on = "\033[1m";
-    const string bold_off = "\033[0m";
+    const string ativarNegrito = "\033[1m";
+    const string desativarNegrito = "\033[0m";
 
 
     // Linha superior da tabela
     cout << "╭─────────┬───────────────┬────────────┬──────────┬──────┬───────────────┬────────────────┬──────────────┬─────────────────┬────────────────────╮" << endl;
-    
+
     // Cabeçalho
     cout << "│"
-         << bold_on
-         << setw(colPlaca) << left << " PLACA "
-         << setw(colBarra) << "│"
-         << setw(colFabricante) << left << " FABRICANTE "
-         << setw(colBarra) << "│"
-         << setw(colModelo) << left << " MODELO "
-         << setw(colBarra) << "│"
-         << setw(colCor) << left << " COR "
-         << setw(colBarra) << "│"
-         << setw(colAno) << left << " ANO "
-         << setw(colBarra) << "│"
-         << setw(colQuilometros) << left << " QUILOMETRAGEM "
-         << setw(colBarra) << "│"
-         << setw(colCategoria) << left << " CATEGORIA "
-         << setw(colBarra) << "│"
-         << setw(colPreco) << left << " PREÇO DIÁRIO "
-         << setw(colBarra) << "│"
-         << setw(colDisponibilidade) << left << " DISPONIBILIDADE "
-         << setw(colBarra) << "│"
-         << setw(colLocador) << left << " LOCADOR "
-         << bold_off
-         << "│" << endl;
-    
+            << ativarNegrito
+            << setw(colPlaca) << left << " PLACA "
+            << setw(colBarra) << "│"
+            << setw(colFabricante) << left << " FABRICANTE "
+            << setw(colBarra) << "│"
+            << setw(colModelo) << left << " MODELO "
+            << setw(colBarra) << "│"
+            << setw(colCor) << left << " COR "
+            << setw(colBarra) << "│"
+            << setw(colAno) << left << " ANO "
+            << setw(colBarra) << "│"
+            << setw(colQuilometros) << left << " QUILOMETRAGEM "
+            << setw(colBarra) << "│"
+            << setw(colCategoria) << left << " CATEGORIA "
+            << setw(colBarra) << "│"
+            << setw(colPreco) << left << " PREÇO DIÁRIO "
+            << setw(colBarra) << "│"
+            << setw(colDisponibilidade) << left << " DISPONIBILIDADE "
+            << setw(colBarra) << "│"
+            << setw(colLocador) << left << " LOCADOR "
+            << desativarNegrito
+            << "│" << endl;
+
     cout << "├─────────┼───────────────┼────────────┼──────────┼──────┼───────────────┼────────────────┼──────────────┼─────────────────┼────────────────────┤" << endl;
 
     // Exibir os dados das 20 linhas
     for (int i = 0; i < tamanho-1; i++) {
         cout << "│"
-             << setw(colPlaca) << left << carros[i].placa
-             << setw(colBarra) << "│"
-             << setw(colFabricante) << left << carros[i].fabricante
-             << setw(colBarra) << "│"
-             << setw(colModelo) << left << carros[i].modelo
-             << setw(colBarra) << "│"
-             << setw(colCor) << left << carros[i].cor
-             << setw(colBarra) << "│"
-             << setw(colAno) << left << carros[i].ano
-             << setw(colBarra) << "│"
-             << setw(colQuilometros) << left << to_string(carros[i].quilometragem) + " Km"
-             << setw(colBarra) << "│"
-             << setw(colCategoria) << left << carros[i].categoria
-             << setw(colBarra) << "│"
-             << setw(colBarra) << "R$ "
-             << setw(colPreco-3) << left << fixed << setprecision(2) << carros[i].preco
-             << setw(colBarra) << "│"
-             << setw(colDisponibilidade) << left << carros[i].disponibilidade
-             << setw(colBarra) << "│"
-             << setw(colLocador) << left << carros[i].locador
-             << "│" << endl;
+                << setw(colPlaca) << left << carros[i].placa
+                << setw(colBarra) << "│"
+                << setw(colFabricante) << left << carros[i].fabricante
+                << setw(colBarra) << "│"
+                << setw(colModelo) << left << carros[i].modelo
+                << setw(colBarra) << "│"
+                << setw(colCor) << left << carros[i].cor
+                << setw(colBarra) << "│"
+                << setw(colAno) << left << carros[i].ano
+                << setw(colBarra) << "│"
+                << setw(colQuilometros) << left << to_string(carros[i].quilometragem) + " Km"
+                << setw(colBarra) << "│"
+                << setw(colCategoria) << left << carros[i].categoria
+                << setw(colBarra) << "│"
+                << setw(colBarra) << "R$ "
+                << setw(colPreco-3) << left << fixed << setprecision(2) << carros[i].preco
+                << setw(colBarra) << "│"
+                << setw(colDisponibilidade) << left << carros[i].disponibilidade
+                << setw(colBarra) << "│"
+                << setw(colLocador) << left << carros[i].locador
+                << "│" << endl;
 
         // Exibir a linha de separação após cada linha
         if (i == tamanho-2)
             cout << "╰─────────┴───────────────┴────────────┴──────────┴──────┴───────────────┴────────────────┴──────────────┴─────────────────┴────────────────────╯" << endl;
         else
             cout << "├─────────┼───────────────┼────────────┼──────────┼──────┼───────────────┼────────────────┼──────────────┼─────────────────┼────────────────────┤" << endl;
-  
+
     }
     for (int i=0; i<tamanho-1; i++){
         cout << "Carro " << i+1 << ": ";
         cout << carros[i].descricao << endl;
     }
 
-    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <typeinfo>
+
+#include "telas.cpp"
+#include "ler.cpp"
 
 using namespace std;
 
 int main() {
-    
-    cout << "Olá commit";
-    // Comentário para branch de teste 1
-    cout << "Esse cout está na brach 1";
 
-    // Comentário na branch master
-    cout << "Esse cout está na branch master";
-    
+    string opcaoPrincipal = telaPrincipal();
+
+    if (opcaoPrincipal == "3"){
+        telaFinal();
+        return 0;
+    }
+
+    int linhas = 0;
+    bool leu = false;
+    leu = csvParaBinario(linhas);
+
+    if (leu){
+        veiculo *carros = new veiculo[40];
+        leu = lerDados(carros, linhas);
+        if (leu)
+            imprimirTabela(carros, linhas);
+        else 
+            return 1;
+    }
+    else
+        return 1;
     return 0;
 }
